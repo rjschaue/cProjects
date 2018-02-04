67de4e140c24722e60f964849e24abd5192b86d2
Robert Joseph Schauer Jr <rjschaue@engr-ras-102.eos.ncsu.edu>
2018-02-04 17:44:11 -0500

    Added reading from standard input to triangle

---
+++ p2/triangle.c
@@ -0,0 +1,78 @@
+/**
+  @file triangle.c
+  @author Joey Schauer (rjschaue)
+
+  This program will take integer and floating point values from standard
+  input that represent a triangle. The first two are integers for width and 
+  height of image. The next six are floating point values for each vertex of
+  the triangle. The last 3 integers are RGB color values for the triangle.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int width;
+    int height;
+
+    if ( !scanf("%d", width) || width < 0) {
+        return EXIT_FAILURE;
+    }
+
+    if ( !scanf("%d", height) || height < 0) {
+        return EXIT_FAILURE;
+    }
+
+    double x1;
+    double y1;
+
+    if ( !scanf("%lf", x1)) {
+        return EXIT_FAILURE;
+    }
+
+    if ( !scanf("%lf", y1)) {
+        return EXIT_FAILURE;
+    }
+
+    double x2;
+    double y2;
+
+    if ( !scanf("%lf", x2)){
+        return EXIT_FAILURE;
+    }
+
+    if ( !scanf("%lf", y2)) {
+        return EXIT_FAILURE;
+    }
+
+    double x3;
+    double y3;
+
+    if ( !scanf("%lf", x3)) {
+        return EXIT_FAILURE;
+    }
+
+    if ( !scanf("%lf", y3)) {
+        return EXIT_FAILURE;
+    }
+
+    int red;
+    int green;
+    int blue;
+
+    if ( !scanf("%d", red) || red < 0 || red > 255) {
+        return EXIT_FAILURE;
+    }
+
+    if ( !scanf("%d", green) || green < 0 || green > 255) {
+        return EXIT_FAILURE;
+    }
+    
+    if ( !scanf("%d", blue) || blue < 0 || blue > 255) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
