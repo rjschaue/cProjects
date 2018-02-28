630b175b996c64f67d72e9346497fbdb558de750
Robert Joseph Schauer Jr <rjschaue@engr-ras-201.eos.ncsu.edu>
2018-02-28 16:42:15 -0500

    Added more comments in the program.

--- p3/hangman.c
+++ p3/hangman.c
@@ -27,6 +27,7 @@
     @return program exit status
 */
 int main( int argc, char *argv[] ) {
+    //Error testing for incorrect number of command line arguments
     if (argc < 2) {
         fprintf(stderr, "usage: hangman <word-file> [seed]\n");
         return EXIT_FAILURE;
@@ -34,9 +35,11 @@ int main( int argc, char *argv[] ) {
         fprintf(stderr, "usage: hangman <word-file> [seed]\n");
         return EXIT_FAILURE;
     }
-     
+    
+    //Adds words from file to global word list 
     readWords( argv[1] );
 
+    //Seeds the randomizer 
     int seed = 0;
     if (argc > 2) {
         seed = atoi(argv[2]);
@@ -49,18 +52,22 @@ int main( int argc, char *argv[] ) {
     }
     
     printf("\n");
+    //Outer while loop to initialize hangman word and letter array
     while (true) {
+        //Chooses word for hangman game
         int index = rand() % wordCount;
-
+        
         char word[WORD_LENGTH_MAX];
         strcpy(word,  words[index]);
 
+        //Initializes the array of possible letters to choose from
         int letterLength = NUMBER_OF_LETTERS;
         char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                           'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
                           'y', 'z'};
-
+        
+        //Creates a temporary word to be filled in as letters are guessed
         int wordLength = strlen(word + 1);
         char tempWord[wordLength];
         strcpy(tempWord, word);
@@ -70,15 +77,20 @@ int main( int argc, char *argv[] ) {
 
         int numberOfParts = 0;
         
+        //Loop for the hangman parts, printing letters and user success/failure
         while (true) {
+            //Prints hangman figure and outputs if user fails
             displayFigure(numberOfParts);            
             if (numberOfParts == MAX_PARTS) {
                 printf("You lose!\n");
                 printf("Word was %s\n\n", word);
                 break;
             }
+            //Prints word with correct guesses
             displayWord(tempWord);
             printf("\n");
+
+            //Prints remaining letters to choose from
             printf("Remaining letters: ");
             for (int i = 0; i < letterLength; i++) {
                 printf("%c", letters[i]);
@@ -90,15 +102,21 @@ int main( int argc, char *argv[] ) {
             
             char letter[MAX_WORDS] = "";
             bool letterFound = false;
+            
+            //Checks user input for letter guesses
             while (true) {
+                //Prompts user for guess and exits if EOF
                 printf("letter> ");
                 if (scanf("%s", letter) == EOF) {
                     return EXIT_SUCCESS;
                 }
                 printf("\n");
+                
+                //Checks for invalid input
                 if (strlen(letter) > 1) {
                     printf("Invalid letter\n\n");
                 } else {
+                    //Checks to see if letter is valid
                     for (int i = 0; i < letterLength; i++) {
                         if (letters[i] == letter[0]) {
                             letterLength--;
@@ -109,6 +127,7 @@ int main( int argc, char *argv[] ) {
                             break;
                         }
                     }
+                    //Breaks loop if a valid letter is found
                     if (letterFound) {
                         break;
                     } else {
@@ -116,6 +135,7 @@ int main( int argc, char *argv[] ) {
                     }
                 }
             }
+            //Checks word against chosen letter to see if it is correct
             bool letterMatch = false;
             for (int i = 0; word[i]; i++) {
                 if (word[i] == letter[0]) {
@@ -125,6 +145,7 @@ int main( int argc, char *argv[] ) {
                     numberOfParts++;
                 }
             }
+            //Tells user they win if word is completed
             if (strcmp(word, tempWord) == 0) {
                 displayWord(word);
                 printf("\n");
@@ -132,6 +153,8 @@ int main( int argc, char *argv[] ) {
                 break;
             }
         } 
+        //Prompts user if they want to play again, exiting for anything other
+        //than a 'y' or 'Y'
         printf("Play again(y,n)> ");
         char terminate[MAX_WORDS] = "";
         scanf("%s", terminate);
